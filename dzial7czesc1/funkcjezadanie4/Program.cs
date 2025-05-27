2e965429e3de4f6b996844c9058e20787a59e4c1
Sebastian Barański <sebastian.baranski3@wp.pl>
2025-05-27 12:25:27 +0200

    Add files via upload

---
+++ dzial7czesc1/funkcjezadanie4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Napisz funkcję o nazwie ile_cyfr, która wyznacza liczbę cyfr dla zadanej liczby naturalnej jako
+//argument tej funkcji. Napisz program wykorzystujący funkcję ile_cyfr.
+namespace funkcjezadanie4
+{
+    internal class Program
+    {
+        static double funkcja(int a)
+
+        { int counter = 0;
+            if (a > 10)
+            {
+                do
+                {
+                    a /= 10;
+                    counter++;
+
+                } while (a > 10);
+                return counter + 1;
+            }
+            else
+                return 1 ;
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            int g;487666562
+                ''''''[]
+            Console.WriteLine("Podaj liczbę naturalną");
+            g = Convert.ToInt32(Console.ReadLine());    
+            Console.WriteLine( " Liczba {0} jest {1}-cyfrową liczbą", g, funkcja(g));
+        }
+    } 
+}
---
+++ dzial7czesc1/funkcjezadanie5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Napisz funkcję, która sprawdza czy jej argument jest liczbą pierwszą. Jako wartość funkcja
+//powinna zwracać 1 dla liczb pierwszych i 0 dla pozostałych liczb. Przetestuj działanie
+//utworzonej funkcji.
+namespace funkcjezadanie5
+{
+    internal class Program
+    {
+        static int pierwsza(int liczba)
+        {
+            int i;
+            for (i = 2; i < liczba / 2; i++) 
+            
+                if (liczba % i == 0) return 0;
+                return 1;
+            
+            
+        
+        
+        
+        }
+        
+        
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());   
+            Console.WriteLine("{0}", pierwsza(a));
+        }
+    }
+}

04736cdf45c8d7009fb53f6d9ab5ec4c4c750234
Sebastian Barański <sebastian.baranski3@wp.pl>
2025-05-27 12:15:26 +0200

    Add files via upload

---
+++ dzial7czesc1/funkcjezadanie7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funkcjezadanie7
+{
+   // Napisz funkcję posiadającą dwa argumenty: p, q – długości przekątnych deltoidu. Funkcja ma
+  //  zwracać pole deltoidu dla zadanych argumentów.Napisz program testujący utworzoną
+ //   funkcję.
+ 
+    internal class Program
+    {
+        static int deltoid(int p, int q)
+        {
+            return ( p * q / 2);
+
+        }
+        
+        
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pole deltoidu o przekątnych 4 i 8 to {0}", deltoid(4, 8));
+        }
+    }
+}

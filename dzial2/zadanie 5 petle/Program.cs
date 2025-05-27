e7a85fd8aec4e756bc0d81d21b36a44177d919b6
Sebastian Barański <sebastian.baranski3@wp.pl>
2025-05-27 12:02:46 +0200

    Add files via upload

---
+++ dzial2/zadanie 5 petle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadaniepetle5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int i, j, n;
+                Console.WriteLine("Podaj liczbq naturalnq n>1: ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                for (i = 1; i <= n; i++)
+                {
+
+                    for (j = 1; j <= i; j++)
+                        Console.Write("*");
+                    Console.WriteLine("");
+                }
+ 
+            Console.ReadKey(true);
+            }
+    }
+}
---
+++ dzial2/zadanie1petle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Napisać program wypisujący na ekranie kolejne liczby całkowite od 11 do 33. Użyj pętli for.
+namespace zadanie1petle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 11; i <= 33; i++)
+            {
+                Console.Write("{0} ", i);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
